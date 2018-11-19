24caa8e95900f09da00458e60dbcafbc82154d35
= <=>
2018-11-18 19:43:13 -0500

    @preauthorized added

--- src/main/java/com/hrms/soen6841/soen6841hrmsspringbootmysql/controller/EmployeeController.java
+++ src/main/java/com/hrms/soen6841/soen6841hrmsspringbootmysql/controller/EmployeeController.java
@@ -5,14 +5,17 @@ import com.hrms.soen6841.soen6841hrmsspringbootmysql.repository.EmployeeReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @GetMapping("/employee")
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public Page<Employee> getEmployee(Pageable pageable) {
         return employeeRepository.findAll(pageable);
     }
