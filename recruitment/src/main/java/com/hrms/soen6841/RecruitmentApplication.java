799484d761c54f9661d547f0553e2ff51aff3714
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2018-11-27 03:52:04 -0500

    Added the recruitment service
    added packadges and application properties running on port 8200

---
+++ recruitment/src/main/java/com/hrms/soen6841/RecruitmentApplication.java
@@ -0,0 +1,17 @@
+package com.hrms.soen6841;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableJpaAuditing
+public class RecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentApplication.class, args);
+	}
+}
